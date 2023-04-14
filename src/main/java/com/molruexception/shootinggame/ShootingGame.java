839c901d1b540ea976b39851ff89dd3f7afad7da
Lede-dev <lede.7716@gmail.com>
2023-04-14 19:57:53 +0900

    Feat: 플러그인 활성화 시 이벤트 리스너 등록

--- src/main/java/com/molruexception/shootinggame/ShootingGame.java
+++ src/main/java/com/molruexception/shootinggame/ShootingGame.java
@@ -1,26 +1,25 @@
 package com.molruexception.shootinggame;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ShootingGame extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
         // Register Command
-        PluginCommand shootCommand = getCommand("shoot");
+        final PluginCommand shootCommand = getCommand("shoot");
         if (shootCommand != null) {
             GameCommand command = new GameCommand();
             shootCommand.setExecutor(command);
             shootCommand.setTabCompleter(command);
         }
-    }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        // Register Listener
+        final PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new GameListener(), this);
     }
 
 }
